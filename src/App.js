dc4c0b96d9661a4d009bf26b9bd25478376d5ef1
danferth <danferth@gmail.com>
2020-12-01 15:59:55 -0800

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
-import Card from "./components/Card";
-import DarkSwitch from "./components/DarkBtn";
-import { useTheme } from "./util/ThemeContext";
 
 const App = () => {
-  const dark = useTheme();
   return (
-    <div className={dark ? `dark` : ``}>
-      <DarkSwitch />
-      <div className="min-w-full min-h-screen flex items-center justify-center z-10 bg-white dark:bg-gray-800 transition">
-        <Card />
-      </div>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,15 +4,11 @@ import App from "./App";
 import SEO from "../src/components/SEO";
 import "./global.output.css";
 
-import { ThemeProvider } from "./util/ThemeContext";
-
 const root = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <SEO title="danferth.dev" />
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <SEO title="danferth" />
+    <App />
   </React.StrictMode>,
   root
 );
